abddcab993aea170433dbf5ff0faed9139dfb169
Gohar00 <107225796+Gohar00@users.noreply.github.com>
2023-05-14 13:24:09 +0400

    Delete 977. Squares of a Sorted Array.py

--- 977. Squares of a Sorted Array.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        nums1 = []
-        for i in nums:
-            nums1.append(i ** 2)
-        return sorted(nums1)
