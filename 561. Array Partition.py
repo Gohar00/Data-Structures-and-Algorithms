9439762ce78854530eb36ac254e53e77990c4df7
Gohar00 <107225796+Gohar00@users.noreply.github.com>
2023-05-14 13:24:01 +0400

    Delete 561. Array Partition.py

--- 561. Array Partition.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def arrayPairSum(self, nums: List[int]) -> int:
-         nums = sorted(nums)
-         summ = 0
-         for i in range(0,len(nums),2):
-            summ += nums[i]
-         return summ
