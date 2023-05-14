a59d8d9ecaaa70e10b66466e218293f8b9b49c00
Gohar00 <107225796+Gohar00@users.noreply.github.com>
2023-05-14 13:23:13 +0400

    Delete 1346. Check If N and Its Double Exist.py

--- 1346. Check If N and Its Double Exist.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def checkIfExist(self, nums: List[int]) -> bool:
-        extra_arr = [2*i for i in nums]
-        for i in range(0,len(nums)):
-            if nums[i] in extra_arr and i != extra_arr.index(nums[i]):
-                return True
-        return False
